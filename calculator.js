c2cbd482c08f4a8cfc9a6093bb40e0caeebbbc78
jc-2502 <16088146+jc-2502@users.noreply.github.com>
2025-12-22 16:47:04 -0500

    Add functions used to display clicked numbers
    Add function to add event listeners to buttons
    Event listeners run function to display clicked numbers
    Currently using num1 variable to store clicked numbers

--- calculator.js
+++ calculator.js
@@ -30,3 +30,30 @@ function operate (operator, num1, num2) {
       return divide(num1, num2);
   }
 }
+
+function addEventListenersToButtons() {
+  const buttons = document.querySelectorAll("button");
+
+  buttons.forEach(button => button.addEventListener("click", displayInput));
+}
+
+function displayInput(event) {
+  const display = document.querySelector("#display");
+  const clickedButton = event.target;
+  let clickedNumber;
+
+  if (clickedButton.classList.contains("number-button")) {
+    clickedNumber = clickedButton.textContent;
+
+    if (num1) {
+      num1 += clickedNumber;
+    } else if (clickedNumber !== "0") {
+      // num1 doesn't have value
+      num1 = clickedNumber;
+    }
+
+    if (num1) display.textContent = num1;
+  }
+}
+
+addEventListenersToButtons();
